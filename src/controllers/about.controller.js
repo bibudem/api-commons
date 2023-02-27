5e45f37a1f900ce714ca9a3083277dce71edc067
Christian Rémillard <christian.remillard@gmail.com>
2023-02-27 10:04:58 -0500

    fix: Ajout d'un "/" à la fin de la route /api-doc/

--- src/controllers/about.controller.js
+++ src/controllers/about.controller.js
@@ -1,6 +1,6 @@
 export function getAboutController(pkg) {
   return function getAbout(req, res) {
     res.send(`<pre>${pkg.name} v${pkg.version} (${process.env.NODE_ENV})
-<p><a href="api-doc">Documentation</a> | <a href="${pkg.homepage}">Sources</a></p></pre>`)
+<p><a href="api-doc/">Documentation</a> | <a href="${pkg.homepage}">Sources</a></p></pre>`)
   }
 }
